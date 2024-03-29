114b90184342f69fcd64879c98cc9205223f2345
nadiraturgunalieva <go19straight@gmail.com>
2024-03-29 14:33:59 +0300

    Определение класса Shape

---
+++ Shape.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+#ifndef SHAPE_H
+#define SHAPE_H
+
+class Shape {
+private:
+
+public:
+	virtual void Scale(float ScaleFactor) = 0;
+	virtual void ShowInfo() = 0;
+	virtual std::string GetName() = 0;
+};
+
+#endif
+
